6dc9f247cd488503b71c97d1824ed529d995e8e5
shepherd <shepherdsoft@outlook.com>
2023-01-28 23:17:51 +0000

    Deleted unnecesary files

--- fcm/.fcm.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/root/YAD/fcm/fcm.mod := printf '%s\n'   flight_control.o fcm_ops.o | awk '!x[$$0]++ { print("/root/YAD/fcm/"$$0) }' > /root/YAD/fcm/fcm.mod
--- fcm/fcm.mod
+++
@@ -1,2 +0,0 @@
-/root/YAD/fcm/flight_control.o
-/root/YAD/fcm/fcm_ops.o
